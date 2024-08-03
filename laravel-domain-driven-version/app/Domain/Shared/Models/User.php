212a05031d141aa1c94dfb2ed50d06d778bba742
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-04 03:19:50 +0700

    feat(database): structure pondation

--- laravel-domain-driven-version/app/Domain/Shared/Models/User.php
+++ laravel-domain-driven-version/app/Domain/Shared/Models/User.php
@@ -4,10 +4,12 @@ namespace Domain\Shared\Models;
 
 use Domain\Shared\Models\BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends BaseModel
 {
-    use SoftDeletes;
+    use SoftDeletes, HasApiTokens;
 
     protected $casts = [];
 }
