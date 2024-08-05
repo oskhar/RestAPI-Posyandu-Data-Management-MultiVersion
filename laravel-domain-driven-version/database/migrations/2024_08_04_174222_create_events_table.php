0d3c0c6c879368288f41d9ff0ec5a26f90ca19b8
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-05 21:43:44 +0700

    fix(database): type and atribute

--- laravel-domain-driven-version/database/migrations/2024_08_04_174222_create_events_table.php
+++ laravel-domain-driven-version/database/migrations/2024_08_04_174222_create_events_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,10 +16,12 @@ return new class extends Migration
             $table->id();
             $table->foreignId('admin_id')->constrained();
             $table->string('title');
+            $table->string('image')->nullable();
             $table->text('description');
             $table->dateTime('start_datetime');
             $table->dateTime('end_datetime')->nullable();
             $table->text('location')->nullable();
+            $table->string('overview');
             $table->timestamps();
             $table->softDeletes();
         });
