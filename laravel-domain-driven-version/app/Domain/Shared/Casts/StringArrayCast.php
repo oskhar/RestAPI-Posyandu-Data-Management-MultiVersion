9c6a220ab7a541fcbf10ba7c2697316ab9cf0123
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-07 02:06:33 +0700

    fix(laravel): depedencies inject

--- laravel-domain-driven-version/app/Domain/Shared/Casts/StringArrayCast.php
+++ laravel-domain-driven-version/app/Domain/Shared/Casts/StringArrayCast.php
@@ -2,14 +2,36 @@
 
 namespace Domain\Shared\Casts;
 
-use Domain\Shared\Data\APIResponseData;
-use Domain\Shared\Enums\APIResponseEnum;
+use App\Infrastructure\API\Data\APIResponseData;
+use App\Infrastructure\API\Enums\APIStatusEnum;
+use App\Infrastructure\Services\APIResponseService;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Spatie\LaravelData\Casts\Cast;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Support\Creation\CreationContext;
 
 class StringArrayCast implements Cast
 {
+    protected APIResponseService $apiResponse;
+
+    /**
+     * Create a new service instance.
+     * @param \App\Infrastructure\Services\APIResponseService $apiResponse
+     */
+    public function __construct(APIResponseService $apiResponse)
+    {
+        $this->apiResponse = $apiResponse;
+    }
+
+    /**
+     * Summary of cast
+     * @param \Spatie\LaravelData\Support\DataProperty $property
+     * @param mixed $value
+     * @param array $properties
+     * @param \Spatie\LaravelData\Support\Creation\CreationContext $context
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     * @return mixed
+     */
     public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
     {
         $errors = [];
@@ -29,11 +51,11 @@ class StringArrayCast implements Cast
         }
 
         if (!empty($errors)) {
-            throw APIResponseEnum::BAD_REQUEST->generate(
-                APIResponseData::from(
-                    status: false,
-                    errors: $errors
-                )
+            throw new HttpResponseException(
+                $this->apiResponse->execute(APIResponseData::from([
+                    "status" => false,
+                    "errors" => $errors,
+                ]), APIStatusEnum::BAD_REQUEST)
             );
         }
 
