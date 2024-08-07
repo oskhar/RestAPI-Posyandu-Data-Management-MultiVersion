d813e81ebc84554018573ac8310a7dd946afbb4d
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-07 19:34:00 +0700

    fix(laravel): specific auth model

--- laravel-domain-driven-version/config/auth.php
+++ laravel-domain-driven-version/config/auth.php
@@ -1,4 +1,5 @@
 <?php
+use Domain\User\Models\User;
 
 return [
 
@@ -62,7 +63,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env('AUTH_MODEL', User::class),
         ],
 
         // 'users' => [
