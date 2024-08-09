69ca9b6f48515e93b402c62add78b36f6d802018
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-09 09:40:27 +0700

    feat(database): member seeder

--- laravel-domain-driven-version/database/seeders/DatabaseSeeder.php
+++ laravel-domain-driven-version/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             JobTitleSeeder::class,
             AdminSeeder::class,
+            MemberSeeder::class
         ]);
     }
 }
