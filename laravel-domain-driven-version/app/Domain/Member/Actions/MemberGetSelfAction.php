6d09e371f0a084ac01d33d69749e7ec205b2433e
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-09 09:29:39 +0700

    feat(laravel): endpoint post member/me

---
+++ laravel-domain-driven-version/app/Domain/Member/Actions/MemberGetSelfAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Domain\Member\Actions;
+
+use App\Infrastructure\API\Data\APIResponseData;
+use App\Infrastructure\API\Enums\APIStatusEnum;
+use App\Infrastructure\Exceptions\APIResponseException;
+use App\Infrastructure\Services\APIResponseService;
+use Domain\Member\Data\MemberData;
+use Illuminate\Http\JsonResponse;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class MemberGetSelfAction
+{
+    use AsAction;
+
+    protected $errors = [];
+    protected APIResponseService $response;
+
+    /**
+     * Create a new service instance.
+     * @param \App\Infrastructure\Services\APIResponseService $response
+     */
+    public function __construct(APIResponseService $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Run the business processes.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function asController(): JsonResponse
+    {
+        $member = $this->execute();
+
+        return ($this->response)(
+            APIResponseData::from([
+                "data" => [
+                    "member" => $member
+                ]
+            ])
+        );
+    }
+
+    /**
+     * Execute the business logic.
+     * @return MemberData
+     */
+    public function execute(): MemberData
+    {
+        return MemberData::fromAuth();
+    }
+}
