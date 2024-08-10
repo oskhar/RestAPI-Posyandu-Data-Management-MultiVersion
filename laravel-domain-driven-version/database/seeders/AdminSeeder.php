997e13e49f71f06a61b6914c351a0bb80a0f3087
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-11 03:04:03 +0700

    feat(database): admin dummy

--- laravel-domain-driven-version/database/seeders/AdminSeeder.php
+++ laravel-domain-driven-version/database/seeders/AdminSeeder.php
@@ -6,6 +6,7 @@ use DB;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class AdminSeeder extends Seeder
 {
@@ -34,6 +35,39 @@ class AdminSeeder extends Seeder
             'user_id' => $userId,
             'job_title_id' => 2,
             'address' => '123 Admin St, Admin City, Admin State, 12345',
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null,
+        ]);
+
+        $faker = Faker::create();
+        for ($i = 0; $i < 25; $i++) {
+            $this->costumeFactory($faker);
+        }
+    }
+    public function costumeFactory($faker): void
+    {
+        $userId = DB::table('users')->insertGetId([
+            'full_name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'password' => Hash::make('TesPassword123'), // Static password for seeding
+            'role' => 'admin',
+            'gender' => $faker->randomElement(['l', 'p']),
+            'profile_picture' => null,
+            'phone_number' => $faker->phoneNumber,
+            'birth_date' => $faker->date('Y-m-d', '2000-01-01'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null,
+        ]);
+
+        // Insert Admin Data
+        DB::table('admins')->insert([
+            'user_id' => $userId,
+            'job_title_id' => $faker->numberBetween(1, 10), // Assuming job titles are between 1 and 10
+            'address' => $faker->address,
             'created_at' => now(),
             'updated_at' => now(),
             'deleted_at' => null,
