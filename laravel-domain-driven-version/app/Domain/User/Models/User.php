37fcb12fc61610837eed22bac43b7a4c20849d69
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-15 15:15:40 +0700

    perf(database): stable user model

--- laravel-domain-driven-version/app/Domain/User/Models/User.php
+++ laravel-domain-driven-version/app/Domain/User/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace Domain\User\Models;
 
+use Domain\Shared\Casts\DateCast;
 use Domain\Shared\Models\BaseModel;
+use Domain\User\Casts\GenderCast;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -13,16 +15,33 @@ class User extends BaseModel
     use SoftDeletes, HasApiTokens, Notifiable;
 
     /**
+     * Costuming cast atribute
+     * @var array
+     */
+    protected $casts = [
+        'gender' => GenderCast::class,
+        'birth_date' => DateCast::class,
+    ];
+
+    /**
+     * Summary of hidden
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
      * Atribut atau kolom yang boleh diubah.
      *
      * @var array<int, string>
      */
     protected $fillable = [
         'full_name',
-        'email',
         'password',
         'role',
         'gender',
+        'language',
         'profile_picture',
         'phone_number',
         'birth_date',
