448973ee2c42175cd05e713ddb0b989ac86a057e
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-16 01:07:58 +0700

    feat(laravel): endpoint post admin

--- laravel-domain-driven-version/app/Domain/User/Actions/Admin/CreateAdminAction.php
+++ laravel-domain-driven-version/app/Domain/User/Actions/Admin/CreateAdminAction.php
@@ -5,6 +5,10 @@ namespace Domain\User\Actions\Admin;
 use App\Infrastructure\API\Data\APIResponseData;
 use App\Infrastructure\Services\APIResponseService;
 use Domain\User\Data\AdminData;
+use Domain\User\Data\UserData;
+use Domain\User\Models\Admin;
+use Domain\User\Models\JobTitle;
+use Domain\User\Models\User;
 use Illuminate\Http\JsonResponse;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -27,9 +31,9 @@ class CreateAdminAction
      * Run the business processes.
      * @return \Illuminate\Http\JsonResponse
      */
-    public function asController(AdminData $data): JsonResponse
+    public function asController(UserData $userData, AdminData $adminData): JsonResponse
     {
-        $this->execute($data);
+        $this->execute($userData, $adminData);
 
         return ($this->response)(
             APIResponseData::from()
@@ -40,8 +44,19 @@ class CreateAdminAction
      * Execute the business logic.
      * @return void
      */
-    public function execute(AdminData $data): void
+    public function execute(UserData $userData, AdminData $adminData): void
     {
+        $userData = $userData->availableEmailValidation();
 
+        $user = User::create([
+            ...$userData->toArray(),
+            "role" => "admin"
+        ]);
+
+        Admin::create([
+            "user_id" => $user->id,
+            "job_title_id" => JobTitle::where("name", $adminData->job_title)->pluck("id")->firstOrFail(),
+            "address" => $adminData->address
+        ]);
     }
 }
