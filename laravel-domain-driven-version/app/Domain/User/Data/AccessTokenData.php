522b3f9ccdfa74af74ff780d86e87255d38f29bb
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-15 15:14:28 +0700

    perf(laravel): sparate business logic from data resource

--- laravel-domain-driven-version/app/Domain/User/Data/AccessTokenData.php
+++ laravel-domain-driven-version/app/Domain/User/Data/AccessTokenData.php
@@ -3,7 +3,6 @@
 namespace Domain\User\Data;
 
 use Carbon\Carbon;
-use Domain\User\Models\User;
 use Spatie\LaravelData\Data;
 
 class AccessTokenData extends Data
@@ -12,19 +11,5 @@ class AccessTokenData extends Data
         readonly ?string $access_token,
         readonly ?Carbon $expires_at,
     ) {
-    }
-
-    public static function fromUserModel(User $user, bool $remember_me = false): self
-    {
-        return self::from([
-            "access_token" => $user->createToken(
-                'personal_access_tokens',
-                ['*'],
-                $expiresAt = Carbon::now()->addMinutes(
-                    config("sanctum." . ($remember_me ? "rm_expiration" : "ac_expiration"))
-                )
-            )->plainTextToken,
-            "expires_at" => $expiresAt,
-        ]);
     }
 }
