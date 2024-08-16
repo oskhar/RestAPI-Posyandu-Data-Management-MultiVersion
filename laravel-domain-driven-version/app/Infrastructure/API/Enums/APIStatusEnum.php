c7bde9058f909a70acdbb0d614f1d8157a583412
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-08-17 00:01:46 +0700

    feat(laravel): 409 conflict exception

--- laravel-domain-driven-version/app/Infrastructure/API/Enums/APIStatusEnum.php
+++ laravel-domain-driven-version/app/Infrastructure/API/Enums/APIStatusEnum.php
@@ -10,6 +10,7 @@ enum APIStatusEnum: int
     case UNAUTHORIZED = 401;
     case FORBIDDEN = 403;
     case NOT_FOUND = 404;
+    case CONFLICT = 409;
     case UNPROCESSABLE_ENTITY = 422;
     case INTERNAL_SERVER_ERROR = 500;
 
@@ -20,8 +21,9 @@ enum APIStatusEnum: int
             self::CREATED => "New entity launched into the cosmos.",
             self::BAD_REQUEST => "Your request veered off course and couldn't escape Earth's gravity!",
             self::UNAUTHORIZED => "Your credentials don't pass the cosmic gatekeeper!",
-            self::FORBIDDEN => "Your request can't travel beyond the space-time boundary!",
+            self::FORBIDDEN => "Your request violates the Sacred Timeline and cannot be fulfilled!",
             self::NOT_FOUND => "The data you're seeking is beyond the bounds of space!",
+            self::CONFLICT => "Collision in the cosmic pathways! Your request encountered a space-time conflict.",
             self::UNPROCESSABLE_ENTITY => "Data anomaly detected. Unable to process your request in this dimension!",
             self::INTERNAL_SERVER_ERROR => "Galactic disruption. An unexpected cosmic event occurred!",
         };
